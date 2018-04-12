47812b53086709072a3d220dada8e323c3b36542
namer1 <38327901+namer1@users.noreply.github.com>
2018-04-12 17:50:48 +0100

    Add files via upload

---
+++ Super Mario.py
@@ -0,0 +1,14 @@
+import pygame as py
+import time
+import random as rd
+py.init()
+
+display_width = 1080
+display_height = 800
+gameDisplay = py.display.set_mode((display_width,display_height)) ## resolution of game
+
+# colours are measured in RGB --> RED, GREEN, BLUE
+black = (0,0,0) # lack of colours
+white = (255,255,255) # all of the colours
+
+py.quit()
