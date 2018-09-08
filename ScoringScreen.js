7b20fbe10d22609d703f94905ba950b51419f106
namer1 <38327901+namer1@users.noreply.github.com>
2018-09-08 11:44:44 +0100

    Delete ScoringScreen.js

--- ScoringScreen.js
+++
@@ -1,9 +0,0 @@
-var scoreScreen = {
-    display : function(){
-        this.canvas = document.getElementById("Game");
-        this.calculateCanvas();
-        var render = this.canvas.getContext("2d");
-        var bg = new Image();
-        bg.src = SCORE_SCREEN_BG;
-    },
-}
