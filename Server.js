38d21c22188d5ce95fe8bf5eee4a02628d7aed81
= <=>
2018-09-16 17:36:22 +0100

    added a function to save the scores in the game

--- Server.js
+++ Server.js
@@ -1,3 +1,15 @@
-fetch("http://localhost").then((response) => response.json()).then(function(result){
-    console.log(result)
-});
+var server = {
+    getScore : function(){
+        return fetch("http://localhost").then((response) => response.json())
+    },
+    saveScore : function(name, score, time){
+        var data = new FormData();
+        data.append('username', name);
+        data.append('points', score);
+        data.append('time', time);
+        return fetch("http://localhost/submit.php", {
+            method: 'post',
+            body: data
+        })
+    }
+}
