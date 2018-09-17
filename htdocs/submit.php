f4469428707b0897d97ae91606a58ac3d370f266
= <=>
2018-09-17 14:52:13 +0100

    Desciding to add more choices of data

--- htdocs/submit.php
+++ htdocs/submit.php
@@ -7,3 +7,5 @@ $points = $_POST["points"]; //find me he information that is in the ""
 
 $database->query("INSERT INTO `scores` (username, time, points) VALUES ('$username', $time, $points)");
 
+// ADD COLLECTION OF DATA OF HOW MANY LOOPS A PLAYER DOES
+// ADD COLLECTION OF DATA OF HOW MUH TIME IS SPENT IN THE AIR & WATER
