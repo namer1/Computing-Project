b9bc8b3eccb26ad5d74c332efddb44f30b09f5c1
= <=>
2018-09-29 19:59:25 +0100

    adding pictures to the game

--- constants.js
+++ constants.js
@@ -1,9 +1,9 @@
 var BACKGROUND_SCROLL_X = 5;
-var BACKGROUND_IMAGE = 'sprites/Background/gameScreen/Background(1).png';
-var BACKGROUND_IMAGE2 = 'sprites/Background/gameScreen/Background(2).png'
+var BACKGROUND_IMAGE = 'sprites/Background/gameScreen/morning/BackgroundMorning(1).png';
+var BACKGROUND_IMAGE2 = 'sprites/Background/gameScreen/morning/BackgroundMorning(2).png'
 var SEA = 'sprites/Background/gameScreen/morning/morningSea.png';
 var MORN_OBJECTS = 'sprites/Background/gameScreen/morning/morningObjects.png';
-var BACKGROUND_IMAGE_CRUSH = 'sprites/Background/gameScreen/crushingWave.png';
+var BACKGROUND_IMAGE_CRUSH = 'sprites/Background/gameScreen/morning/crushingWaveMorning.png';
 var START_SCREEN_BG = 'sprites/Background/startScreen/startSreenBackground.png';
 
 var LEFT_ARROW = 37;
