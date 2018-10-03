1758dad3b1c24bfd81ac93528439c2d10d1100b1
= <=>
2018-10-03 17:57:31 +0100

    Changing the PHP file locaion + Changing the database format

---
+++ php/submit.php
@@ -0,0 +1,17 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+$database = new mysqli("localhost", "root", "123567", "surfing");
+$user_id = $_POST["user_id"];
+$username = $_POST["username"]; //find me the information that is in the ""
+$level = $_POST["level"];
+$points = $_POST["points"];
+
+if($user_id == -1){
+    $database->query("INSERT INTO `users` (username) VALUES ('$username')");
+    $new_userid = $database->insert_id;
+}
+else{
+    $new_userid = $user_id;
+}
+$database->query("INSERT INTO `games` (user_id, points, level) VALUES ($new_userid, $points, $level)");
+echo $new_userid;
