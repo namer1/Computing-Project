aca2991cd80295550caebbf895b5da45055627b3
= <=>
2018-10-14 10:43:52 +0100

    adding top 10 score table

--- GameOver.js
+++ GameOver.js
@@ -10,7 +10,11 @@ var gameOver ={
                 name = 'Anonymous'
             }
         }
-        server.saveScore(name, scoring.score, game.currentLvl+1)
+        server.saveScore(name, scoring.score, game.currentLvl+1).then(function(){
+            server.getScores().then(function(list){
+                this.drawTable(list);
+            }.bind(this))
+        }.bind(this))
     },
     display : function(){
         this.canvas = document.getElementById("Game");
@@ -29,12 +33,39 @@ var gameOver ={
             render.fillText("Game over",50,50);
         }.bind(this)
     },
+    drawTable : function(list){
+        var render = this.canvas.getContext("2d");
+        render.fillStyle = 'white';
+        render.fillRect(200,200,600,600);
+        render.fillStyle = 'black';
+        render.fillText("Player", 250, 250);
+        render.fillText("Points", 450, 250);
+        render.fillText("Level", 650, 250);
+        var find_user = document.cookie.split('user_id=')[1];
+        var user_id = -1;
+        if (find_user) {
+            user_id = find_user.split(';')[0]; // splits the cookie into a list twice and then takes the value of user_id
+        }
+        for (let index = 0; index < list.length; index++) {
+            const element = list[index]; // element is each row we go through
+            if(element.user_id == user_id){
+                render.fillStyle = 'red'
+            }
+            else{
+                render.fillStyle = 'black'
+            }
+            render.fillText(element.username, 250, 300 + 50*index)
+            render.fillText(element.points, 450, 300 + 50*index)
+            render.fillText(element.level, 650, 300 + 50*index)
+
+        }
+    },
     checkStart: function(event) {
         var p = this.getMousePos(event);
     
         if (p.x >= this.rect.x && p.x <= this.rect.x + this.rect.w &&
             p.y >= this.rect.y && p.y <= this.rect.y + this.rect.h) {
-            this.startGame();
+            location.reload();
         }
     },
     getMousePos : function(e) {
--- php/index.php
+++ php/index.php
@@ -2,7 +2,13 @@
 header('Access-Control-Allow-Origin: *');
 $database = new mysqli("localhost", "root", "123567", "surfing");
 
-$all_scores = $database->query("SELECT * FROM `scores`");
+$all_scores = $database->query(
+    "SELECT users.username, games.user_id, games.points, games.level FROM
+    (SELECT user_id, MAX(points) AS points FROM `games`
+    GROUP BY user_id) AS max_scores
+    JOIN `games` ON max_scores.user_id = games.user_id AND max_scores.points = games.points
+    JOIN `users` ON games.user_id = users.user_id
+    ORDER BY points DESC LIMIT 10"); //DESC means in biggest to smallest
 
 $to_print = [];
 while ($row = $all_scores->fetch_assoc()) {
