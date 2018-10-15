42acb6b697557d9338d3396bd09dd8b073da163b
= <=>
2018-10-15 10:55:23 +0100

    trying to add the player's top score even if not in top 10

--- php/index.php
+++ php/index.php
@@ -8,11 +8,24 @@ $all_scores = $database->query(
     GROUP BY user_id) AS max_scores
     JOIN `games` ON max_scores.user_id = games.user_id AND max_scores.points = games.points
     JOIN `users` ON games.user_id = users.user_id
-    ORDER BY points DESC LIMIT 10"); //DESC means in biggest to smallest
+    ORDER BY points DESC LIMIT 10"); //DESC means in biggest to smallest (descending)
 
+// ANOTHER QUERY WHERE IF PLAYER TOP SCORE ISN'T IN TOP 10 IT WILL STILL SHOW
+// BUT ONLY TO PLAYER BELOW THE TOP 10 AND HOW MANY POINTS HE HAS
+$private_score = $database->query(
+    "SELECT users.username, games.user_id, games.points, games.level FROM
+    (SELECT user_id, MAX(points) AS points FROM `games`
+    GROUP BY user_id) AS max_scores
+    WHERE games.user_id = users.user_id"
+)
 $to_print = [];
+$sec_print = [];
 while ($row = $all_scores->fetch_assoc()) {
     $to_print[] = $row; // adding to list
 }
+while ($row = $private_score->fetch_assoc()) {
+    $sec_print[] = $row; // adding to list
+}
 
 echo json_encode($to_print);
+echo json_encode($sec_print);
