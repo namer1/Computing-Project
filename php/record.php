4c0d40d7f0458f43735c9009da49b58bb65ae4a2
= <=>
2018-12-06 20:33:47 +0000

    found the problem in ghost player --> SQL query UPDATE means somethignhas to be lareadt inside it (table was empty)

--- php/record.php
+++ php/record.php
@@ -2,8 +2,11 @@
 header('Access-Control-Allow-Origin: *');
 $database = new mysqli("localhost", "root", "123567", "surfing");
 $file = $_POST["file"];
-print_r($file);
-var_dump($file);
-$database->query("UPDATE `ghostplayer` SET text = '$file', _updated=NOW()");
+
+var_dump($file); // everything in the variable
+$database->query("UPDATE `ghostplayer` SET text = '$file', _updated=NOW()"); // if data is empty than there won;t be any chagnes because UPDATE means that somethng needs to be there
 //something does not work
+if ($database->affected_rows == 0) { // if the prevous query did not work (if database is empty)
+    $database->query("INSERT INTO `ghostplayer` (text, _updated) VALUES ('$file', NOW())");
+}
 ?>
