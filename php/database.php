0f989e3a0c5aaeb5cc966684612e4abd59fd3d0f
= <=>
2018-12-20 20:52:33 +0000

    make database local

--- php/database.php
+++ php/database.php
@@ -4,6 +4,6 @@ $cleardb_server   = $cleardb_url["host"];
 $cleardb_username = $cleardb_url["user"];
 $cleardb_password = $cleardb_url["pass"];
 $cleardb_db       = substr($cleardb_url["path"],1);
-//$database = new mysqli("localhost", "root", "123567", "surfing"); // have the database locally
-$database = new mysqli($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db); // have the database live
+$database = new mysqli("localhost", "root", "123567", "surfing"); // have the database locally
+//$database = new mysqli($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db); // have the database live
 ?>
